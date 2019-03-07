f2c46110148fa74acfc058929efc97c9c6a7e342
dubox <dubox@duboxdeMacBook-Pro.local>
2019-03-07 17:37:12 +0800

    0.5.0

---
+++ public/platform.js
@@ -0,0 +1,29 @@
+if(window.navigator.platform.toLowerCase().indexOf('win') > -1){
+    document.write(`<style>    
+    ::-webkit-scrollbar
+    {
+    width: 0px;
+    height: 15px;
+    }
+    ::-webkit-scrollbar-track:horizontal
+    {
+        height: 50px;
+    background-color: rgba(219, 232, 255,0);
+    -webkit-border-radius: 3px;
+    }
+    ::-webkit-scrollbar-track-piece
+    {
+    background-color: rgba(219, 232, 255,0);
+    -webkit-border-radius: 3px;
+    }
+    ::-webkit-scrollbar-thumb:horizontal
+    {
+    -webkit-border-radius: 30px;
+    background-color: rgba(139, 139, 139,0.3);
+    }
+    ::-webkit-scrollbar-thumb:horizontal:hover
+    {
+    background-color: rgba(139, 139, 139,0.5);
+    }
+    </style>`);
+}
