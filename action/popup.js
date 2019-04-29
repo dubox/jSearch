b9cd3445386b5b3e7ad0477fa8002ff9a9a54a8f
dubox <dubox@duboxdeMacBook-Pro.local>
2019-04-29 13:54:31 +0800

    新增:分享到PC

--- action/popup.js
+++ action/popup.js
@@ -1,14 +1,25 @@
 
+var pageInfo = {};
+
     chrome.tabs.getAllInWindow(function(tabs){
         for(let i in tabs){
             if(tabs[i].active){
                 new QRCode(document.getElementById("qr"), tabs[i].url);
+                pageInfo.url = tabs[i].url;
+                pageInfo.title = tabs[i].title;
+                //console.log(tabs[i]);
                 return;
             }
         }
     })
 
 
+    document.getElementById('share').onclick = function(){
+        document.getElementById('share_con').value = pageInfo.title+"\n"+pageInfo.url;
+        document.getElementById('share_con').select();
+        document.execCommand('copy');
+        this.innerHTML='分享到PC(复制成功)';
+    }
     document.getElementById('goJ').onclick = function(){
         window.open(chrome.extension.getURL('/options/search.html'));
     }
