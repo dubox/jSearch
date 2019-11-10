ceceb4174e8ddbfbfd78badb755567f66d511877
dubox <dubox@duboxdeMacBook-Pro.local>
2019-11-11 02:14:55 +0800

    优化站点logo显示方案

--- options/options.js
+++ options/options.js
@@ -1,4 +1,36 @@
 Vue.use(VueDND);
+
+Vue.directive('real-img', async function (el, binding) {//指令名称为：real-img
+    let imgURL = binding.value;//获取图片地址
+    if (imgURL) {
+        let exist = await imageIsExist(imgURL);
+        if (exist) {
+            el.setAttribute('src', imgURL);
+        } 
+    }
+})
+
+/**
+ * 检测图片是否存在
+ * @param url
+ */
+let imageIsExist = function(url) {
+    return new Promise((resolve) => {
+        var img = new Image();
+        img.onload = function () {
+            if (this.complete == true){
+                resolve(true);
+                img = null;
+            }
+        }
+        img.onerror = function () {
+            resolve(false);
+            img = null;
+        }
+        img.src = url;
+    })
+}
+
 var app = new Vue({
     el: '#app',
     data: {
